52b33205f968cfd2dd2242b4667abd792de19f40
Amir <a.em@outlook.com>
2023-04-30 00:15:56 +0200

    Expanding CARDS

--- Expanding cards/script.js
+++
@@ -1,9 +0,0 @@
-const panels = document.querySelectorAll('.panel');
-
-panels.forEach((panel) => panel.addEventListener('click', function () {
-     const allItems = [...panel.parentElement.children];
-     allItems.forEach((item) => item.classList.remove('active'))
-     panel.classList.add('active');
-
-}));
-
