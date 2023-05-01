16d9fb278cb78f8b47511c8cf991aa2177471058
Amir <a.em@outlook.com>
2023-05-01 19:11:14 +0200

    Scroll Animation

---
+++ 7.Scroll Animation/1.initialize all projects/script.js
@@ -0,0 +1,18 @@
+const boxes = document.querySelectorAll('.box');
+
+console.log(boxes)
+
+window.addEventListener('scroll', checkBoxes);
+
+checkBoxes();
+
+function checkBoxes() {
+     const triggerBottom = window.innerHeight * 0.75;
+     boxes.forEach(box => {
+          const boxTop = box.getBoundingClientRect().top
+
+          if (boxTop < triggerBottom) {
+               box.classList.add('show')
+          } else box.classList.remove('show')
+     })
+}
